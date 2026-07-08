c94a0399609be67810b7dab796111bcae3d9ed58
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-08 14:29:56 +0300

    Inject repository in user service
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- backend/src/main/java/com/project/expensemanager/service/UserService.java
+++ backend/src/main/java/com/project/expensemanager/service/UserService.java
@@ -1,7 +1,18 @@
 package com.project.expensemanager.service;
 
+import com.project.expensemanager.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    // Inject repositories
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // Business logic
+    
 }
