de77384a7cf6b1d366687f9b659fa19c23c17a0b
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-09 13:52:37 +0300

    Add category service methods
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- backend/src/main/java/com/project/expensemanager/service/CategoryService.java
+++ backend/src/main/java/com/project/expensemanager/service/CategoryService.java
@@ -1,7 +1,27 @@
 package com.project.expensemanager.service;
 
+import com.project.expensemanager.entity.Category;
+import com.project.expensemanager.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
+    // Inject repository
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    // Read
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Integer categoryId) {
+        return categoryRepository.findById(categoryId).orElseThrow(() -> new RuntimeException("Category not found"));
+    }
 }
