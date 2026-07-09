2a0946766f529460fe0a299c8c1e26415e4455f0
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-09 14:51:43 +0300

    Add create budget method
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- backend/src/main/java/com/project/expensemanager/service/BudgetService.java
+++ backend/src/main/java/com/project/expensemanager/service/BudgetService.java
@@ -1,7 +1,40 @@
 package com.project.expensemanager.service;
 
+import com.project.expensemanager.entity.Budget;
+import com.project.expensemanager.entity.User;
+import com.project.expensemanager.repository.BudgetRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Service
 public class BudgetService {
+    // Inject repositories
+    private final BudgetRepository budgetRepository;
+
+    public BudgetService(BudgetRepository budgetRepository) {
+        this.budgetRepository = budgetRepository;
+    }
+
+    // Business logic
+    // Create
+    public Budget createBudget(User user, BigDecimal amount, LocalDate budgetPeriod) {
+        budgetPeriod = budgetPeriod.withDayOfMonth(1);        // Allow only one monthly budget
+        Budget budget = new Budget(user, amount, budgetPeriod);
+
+        return budgetRepository.save(budget);
+    }
+
+
+    // Read
+
+    // Update
+
+    // Delete
+
+    // Get total of monthly expenses
+
+    // Get monthly remaining budget after expenses
+
 }
