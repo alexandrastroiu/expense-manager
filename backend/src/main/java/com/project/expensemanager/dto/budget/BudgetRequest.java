2c214b78ab06655986be84aa0e463b392837a4a5
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-10 17:46:22 +0300

    Add budget request dto
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ backend/src/main/java/com/project/expensemanager/dto/budget/BudgetRequest.java
@@ -0,0 +1,17 @@
+package com.project.expensemanager.dto.budget;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record BudgetRequest(
+        @NotNull(message = "Amount is required")
+        @Positive
+        BigDecimal amount,
+
+        @NotNull(message = "Budget period is required")
+        LocalDate budgetPeriod
+) {
+}
