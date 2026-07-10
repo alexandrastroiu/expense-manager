e1fb94b6f5cb2d5046d3e809b0f9f7b4feb38dd6
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-10 18:38:15 +0300

    Add budget response dto
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ backend/src/main/java/com/project/expensemanager/dto/budget/BudgetResponse.java
@@ -0,0 +1,16 @@
+package com.project.expensemanager.dto.budget;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record BudgetResponse(
+        Integer id,
+        BigDecimal amount,
+        LocalDate budgetPeriod,
+        BigDecimal totalCurrentExpenses,
+        BigDecimal totalMonthlyExpenses,
+        BigDecimal remainingCurrentBudget,
+        BigDecimal remainingMonthlyBudget,
+        BigDecimal budgetPercentage
+) {
+}
