c8bc042eca4a6d4bbcccdd83e4851242a0bbf891
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-10 21:57:22 +0300

    Delete unused import
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- backend/src/main/java/com/project/expensemanager/service/RecurringExpenseService.java
+++ backend/src/main/java/com/project/expensemanager/service/RecurringExpenseService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Locale;
 
 @Service
 public class RecurringExpenseService {
