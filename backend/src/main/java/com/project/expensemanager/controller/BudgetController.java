8b82a930cad08dfbf57d79ec32d48d0daf3df62d
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-15 15:58:22 +0300

    Add create budget method in controller
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- backend/src/main/java/com/project/expensemanager/controller/BudgetController.java
+++ backend/src/main/java/com/project/expensemanager/controller/BudgetController.java
@@ -1,7 +1,50 @@
 package com.project.expensemanager.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.expensemanager.dto.budget.BudgetRequest;
+import com.project.expensemanager.dto.budget.BudgetResponse;
+import com.project.expensemanager.entity.User;
+import com.project.expensemanager.repository.BudgetRepository;
+import com.project.expensemanager.service.BudgetService;
+import com.project.expensemanager.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/budgets")    // Base URL
 public class BudgetController {
+    private final BudgetService budgetService;
+    private final UserService userService;
+
+    public BudgetController(BudgetService budgetService, UserService userService) {
+        this.budgetService = budgetService;
+        this.userService = userService;
+    }
+
+    // Create
+    @PostMapping
+    public ResponseEntity<BudgetResponse> createBudget(
+            @RequestParam Integer userId,
+            @Valid @RequestBody BudgetRequest request
+            ) {
+        User user = userService.getUserById(userId);
+
+        BudgetResponse budget = budgetService.createBudget(user, request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(budget);
+    }
+
+    // Update
+    @PutMapping
+    public ResponseEntity<> updateBudget() {
+
+    }
+
+    // Delete
+    @DeleteMapping
+    public ResponseEntity<> deleteBudget() {
+
+    }
+
 }
