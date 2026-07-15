5b3bf6d154d49b601c7da523d6be884b613c7bbd
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-15 16:25:27 +0300

    Add getBudgetByPeriod method in budget controller"
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- backend/src/main/java/com/project/expensemanager/controller/BudgetController.java
+++ backend/src/main/java/com/project/expensemanager/controller/BudgetController.java
@@ -7,9 +7,12 @@ import com.project.expensemanager.entity.User;
 import com.project.expensemanager.service.BudgetService;
 import com.project.expensemanager.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/budgets")    // Base URL
@@ -49,7 +52,17 @@ public class BudgetController {
     }
 
     // Get budget by period
-    //@GetMapping
+    @GetMapping
+    public ResponseEntity<BudgetResponse> getBudgetByPeriod(
+            @RequestParam Integer userId,
+            @RequestParam @DateTimeFormat LocalDate period
+            ) {
+            User user = userService.getUserById(userId);
+
+            BudgetResponse budget = budgetService.getUserBudgetByPeriod(user, period);
+
+            return ResponseEntity.status(HttpStatus.OK).body(budget);
+    }
 
     // Budget summary
    // @GetMapping
