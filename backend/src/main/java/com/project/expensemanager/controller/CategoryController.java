2985704856adecbc76e7f413357f9e07f5cdce41
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2026-07-15 15:35:56 +0300

    Update category controller
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- backend/src/main/java/com/project/expensemanager/controller/CategoryController.java
+++ backend/src/main/java/com/project/expensemanager/controller/CategoryController.java
@@ -1,7 +1,28 @@
 package com.project.expensemanager.controller;
 
+import com.project.expensemanager.dto.category.CategoryResponse;
+import com.project.expensemanager.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/categories")   // Base URL
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    // Get all categories
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        List<CategoryResponse> categories = categoryService.getAllCategories();
+
+        return ResponseEntity.status(HttpStatus.OK).body(categories);
+    }
+
 }
